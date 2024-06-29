21fa558db5979d0ee67220281146043e0742bc22
nanako45a <nsz.rnmk120@gmail.com>
2024-06-29 21:41:45 +0900

    ✨Add:オブジェクト、クラスについて

--- 2024/javascript-lesson/script3.js
+++ 2024/javascript-lesson/script3.js
@@ -8,3 +8,20 @@ const greet = function() {
   
 // 関数を呼び出しコンソールに出力
 greet();
+
+// ---------------------------------------
+// ------オブジェクトの作成と出力----------
+// 定数animalを定義しオブジェクトを代入（オブジェクトは、「プロパティ名＋文字列,数値,関数等の値」）
+const animal={
+    name:"レオ",
+    age:3,
+    greet:() => {
+        console.log("こんにちは");
+    }
+};
+
+// 定数animalのnameプロパティの値を出力してください
+console.log(animal.name);
+
+// 定数animalのgreetプロパティの関数を呼び出し
+animal.greet();
