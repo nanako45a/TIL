a9af892ed878eeca40d5b6cf32b14a0bbd90d82b
nanako45a <nsz.rnmk120@gmail.com>
2024-06-29 19:47:45 +0900

    ✨Add:文字列〜定数について

---
+++ 2024/javascript-lesson/script1.js
@@ -0,0 +1,30 @@
+// -------------変数について----------------
+// ---------------------------------------
+// 変数length(半径)を定義してください
+let length = 5;
+// 変数lengthの値をコンソールに出力してください
+console.log(length);
+// 変数lengthを用いて、円の面積を出力してください
+// 円の面積の求め方：「半径×半径×円周率3」
+console.log(length*length*3);
+
+// ---------------------------------------
+
+// 変数nameを定義し、'にんじゃわんこ'を代入
+let name = 'にんじゃわんこ';
+// 変数nameの値をコンソールに出力
+console.log(name);
+// 変数nameの値を"とりずきん"に更新
+name = "とりずきん";
+// 変数nameの値をコンソールに出力
+console.log(name);
+
+// ---------------------------------------
+let number = 7;
+console.log(number);
+// 変数numberの値に3を加えてください
+number = number+3;
+console.log(number);
+// 変数numberの値を2で割ってください
+number = number/2;
+console.log(number);
